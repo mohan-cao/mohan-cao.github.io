3496b5416d2a3c2ea5e6a803f00626e45602b7f9
mohan-cao <mohancao@yahoo.com.au>
2018-12-19 14:27:41 +1300

    Updates

---
+++ precache-manifest.8d2acf01eef926995216799578c0cff6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7c5ddccd1ea5a6ea7cad",
+    "url": "/static/js/main.7c5ddccd.chunk.js"
+  },
+  {
+    "revision": "e67eccefed6d45dcd3de",
+    "url": "/static/js/1.e67eccef.chunk.js"
+  },
+  {
+    "revision": "7c5ddccd1ea5a6ea7cad",
+    "url": "/static/css/main.23c7bdaa.chunk.css"
+  },
+  {
+    "revision": "5481f89fcfec57a5a2158b3c19492ff4",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8895079c5bc848c86e2df6c3899890ae.js"
+  "/precache-manifest.8d2acf01eef926995216799578c0cff6.js"
 );
 
 workbox.clientsClaim();
