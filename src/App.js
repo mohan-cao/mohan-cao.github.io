fc549f1987eb092692cb8f0e67e9298095230c59
mohan-cao <mohancao@yahoo.com.au>
2018-12-19 14:28:49 +1300

    Change background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Container from './Container';
 import Resume from './Resume';
-import './App.css';
+import './App.scss';
+
+const numBokehDots = 20
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div style={{ zIndex: 0, position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'black' }} />
+        { [...new Array(numBokehDots).keys()].map(x => <span key={x} className="bokeh-dot" />)}
+        {/* <div style={{ zIndex: 0, position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'black' }} /> */}
         <Container style={{ zIndex: 1 }}><Resume /></Container>
       </div>
     );
