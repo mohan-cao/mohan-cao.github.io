5e3848153e48e6a268240e5c0907eb755ebac076
mohan-cao <mohancao@yahoo.com.au>
2019-01-31 23:53:56 +1300

    Try and make the build work this time

--- src/component/ProjectsComponent.js
+++ src/component/ProjectsComponent.js
@@ -3,31 +3,34 @@ import { faGithub as GithubIcon } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCodeBranch} from '@fortawesome/free-solid-svg-icons';
 import { faStar } from '@fortawesome/free-regular-svg-icons';
-import ImageLoader from './ImageLoader'
+import ImageLoader from './ImageLoader';
+import './ProjectsComponent.scss';
 
 const DEFAULT_PROJ_DESC = 'No description available. Most likely a university project or something random.';
 
 function GitHubProjectsComponent(proj) {
   return (
-    <div key={proj.id} className="even-columns">
+    <div className="ProjectsComponent even-columns" key={proj.id}>
       <div className="even-columns-child" style={{ overflow: 'hidden', position: 'relative', textAlign: 'center' }}>
         <ImageLoader
           alt={"background screenshot for project " + proj.name}
           className="bg"
           src={(proj.homepage)?`https://image.thum.io/get/${proj.homepage}`:''}
+          unselectable="on" style={{ MozUserSelect: 'none', WebkitUserSelect: 'none', msUserSelect: 'none' }}
         />
         <div className="translucent-bg">
           <h3 style={{ textTransform: 'none' }}>{proj.name.replace(/-/g, ' ').replace(/(^|\s)\S/g, l => l.toUpperCase())}</h3>
           <p><em>{(proj.description && proj.description.length) ? proj.description[0].toUpperCase()+proj.description.slice(1) : DEFAULT_PROJ_DESC}</em></p>
-          <p>
-            {proj.stargazers_count} <FontAwesomeIcon icon={faStar} /> 
-            {proj.forks} <FontAwesomeIcon icon={faCodeBranch} /> 
+          <p unselectable="on" style={{ MozUserSelect: 'none', WebkitUserSelect: 'none', msUserSelect: 'none' }}>
+            {proj.stargazers_count}<FontAwesomeIcon icon={faStar} style={{ marginRight: '0.4em', marginLeft: '0.2em' }} />
+            {proj.forks}<FontAwesomeIcon icon={faCodeBranch} style={{ marginLeft: '0.2em' }} />
             <a style={{ marginLeft: 20 }} href={proj.html_url}><FontAwesomeIcon icon={GithubIcon} /> GitHub</a>
           </p>
           {
             (proj.homepage) ?
             <p style={{ borderTop: '0.5px solid #fff', marginTop: 15 }}>
-              <a href={proj.homepage}>Visit the homepage</a>
+              <a unselectable="on" style={{ MozUserSelect: 'none', WebkitUserSelect: 'none', msUserSelect: 'none' }} href={proj.homepage}>Visit the homepage</a>
+              <span className="invisible">Link: {proj.homepage}</span>
             </p> : ''
           }
         </div>
