4bac2a79399a4e599368848f2b06abc07bdab218
mohan cao <deploy@travis-ci.org>
2019-12-13 10:50:41 +0000

    Deploy mohan-cao/mohan-cao.github.io to github.com/mohan-cao/mohan-cao.github.io.git:master

---
+++ precache-manifest.c4ad5ebff3e497741fd1cab0235a2fc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d662988ef78d5d84b3dfad5ea69b0f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d01386e2f31f3734b36",
+    "url": "/static/css/main.4db5adee.chunk.css"
+  },
+  {
+    "revision": "3f5674b6f223594dfad4",
+    "url": "/static/js/2.32d8f6ed.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.32d8f6ed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d01386e2f31f3734b36",
+    "url": "/static/js/main.e21af3b4.chunk.js"
+  },
+  {
+    "revision": "3d4187dd616883265615",
+    "url": "/static/js/runtime-main.a4342e30.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eaa38cd1249cdc8f01785d2dcf298121.js"
+  "/precache-manifest.c4ad5ebff3e497741fd1cab0235a2fc8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
